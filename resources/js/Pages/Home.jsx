be5c5eb31ce621dcef6b1e8316b58a98739c0b73
Abooo0d <abdsadalden2001@gmail.com>
2024-03-18 11:26:21 +0200

    Clear Unnecessary Routes And View Files

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+
+export default function Home() {
+  return (
+    <>
+      <Head title="Social media Laravel + React" />
+      <div>Test</div>
+    </>
+  );
+}
