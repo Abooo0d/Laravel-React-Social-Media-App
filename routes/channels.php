4304f0e675af3842bb7e20be73ac07c466521796
Abooo0d <abdsadalden2001@gmail.com>
2025-09-06 11:32:26 +0300

    Fixing The Pipline

--- routes/channels.php
+++ routes/channels.php
@@ -15,24 +15,26 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel(
-  'chat.{chatId}',
-  fn($user, $chatId) =>
-  Chat::find($chatId)->users->contains($user->id)
-);
-Broadcast::channel('online', function ($user) {
-  $authUser = Auth::user();
-  if ($authUser && $authUser->isFriend($user->id)) {
-    return new UserResource($user);
-  }
-  return $authUser;
-});
-Broadcast::channel(
-  'user.{id}',
-  fn($user, $id) => (int) $user->id == (int) $id
-);
-Broadcast::channel(
-  'call.{chatId}',
-  fn($user, $chatId) =>
-  Chat::find($chatId)->users->contains($user->id)
-);
+if (app()->environment('production', 'local')) {
+  Broadcast::channel(
+    'chat.{chatId}',
+    fn($user, $chatId) =>
+    Chat::find($chatId)->users->contains($user->id)
+  );
+  Broadcast::channel('online', function ($user) {
+    $authUser = Auth::user();
+    if ($authUser && $authUser->isFriend($user->id)) {
+      return new UserResource($user);
+    }
+    return $authUser;
+  });
+  Broadcast::channel(
+    'user.{id}',
+    fn($user, $id) => (int) $user->id == (int) $id
+  );
+  Broadcast::channel(
+    'call.{chatId}',
+    fn($user, $chatId) =>
+    Chat::find($chatId)->users->contains($user->id)
+  );
+}
