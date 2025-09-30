454bcab8d148800c1e906df467eeba667f6adcef
Abooo0d <abdsadalden2001@gmail.com>
2025-09-30 13:03:00 +0300

    Fixing the signup with google v4

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -21,14 +21,23 @@ class SocialAuthController extends Controller
     // $avatarUrl = $socialUser->getAvatar();
     $name = $socialUser->getName() ?? $socialUser->getNickname();
     $email = $socialUser->getEmail();
-    // Find or create user
-    $user = User::updateOrCreate(
-      [$provider . '_id' => $socialUser->getId()],
-      [
+    $user = User::where('email', $email)->first();
+    if ($user) {
+      // 2. If user exists, attach provider ID if not already set
+      $column = $provider . '_id';
+      if (!$user->$column) {
+        $user->$column = $socialUser->getId();
+        $user->save();
+      }
+    } else {
+      // 3. If no user exists with this email, create a new one
+      $user = User::create([
         'name' => $name,
         'email' => $email,
-      ]
-    );
+        $provider . '_id' => $socialUser->getId(),
+        'avatar' => $socialUser->getAvatar(),
+      ]);
+    }
     Auth::login($user);
     return redirect('/'); // redirect to React/Inertia app home
   }
